05a0bc57b965a07227b71a5dfbda16c3fad31a49
aslan-asilon31 <sulaslansetiawan31@gmail.com>
2024-11-12 12:53:24 +0700

    update

--- resources/views/layouts/layout_auth.blade.php
+++ resources/views/layouts/layout_auth.blade.php
@@ -65,6 +65,7 @@
 <!-- Bootstrap 4 -->
 <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('adminlte/dist/js/adminlte.min.js')}}"></script>
+<script src="{{
+adminlte/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
